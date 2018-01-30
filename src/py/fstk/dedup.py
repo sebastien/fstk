60e060c0877091697dc52adfb53a77810444645c
Sébastien Pierre <sebastien.pierre@gmail.com>
2018-01-30 16:39:53 -0500

    [Update] dedup: Nested files, improved output

--- src/py/fstk/dedup.py
+++ src/py/fstk/dedup.py
@@ -25,38 +25,60 @@ class Dedup(CatalogueReader):
 		if not os.path.exists(self.dbPath):
 			os.mkdir(self.dbPath)
 		# We clean any previous run
-		self.clean()
+		# self.clean()
 		# We start reading
-		self.read(path)
-		# for p in self.listPaths():
-		# 	with open(p,"rt") as f:
-		# 		paths = [_[:-1] for _ in f.readlines()]
-		# 		self.dedup(paths)
+		# self.read(path)
+		for p in self.listSHA1Paths():
+			self.dedup(self.getSHA1Paths(p))
 
-	def listPaths( self ):
-		if os.path.exists(self.dbPath):
-			for p in os.listdir(self.dbPath):
-				p = os.path.join(self.dbPath, p)
-				if os.path.exists(p):
-					yield p
+	def listSHA1Paths( self, parent=None, depth=10 ):
+		"""Iterates through the SHA-1 paths stored here."""
+		parent = parent or self.dbPath
+		if os.path.isdir(parent) and depth >= 0:
+			for name in os.listdir(parent):
+				path = os.path.join(parent, name)
+				if path.endswith(".lst"):
+					yield path
+				else:
+					yield from self.listSHA1Paths(path, depth - 1)
+
+	def getSHA1Paths( self, path ):
+		if os.path.exists(path):
+			with open(path, "rt") as f:
+				return [_[:-1] for _ in f.readlines()]
+		else:
+			return ()
+
+	def getPathForSHA1( self, sha1 ):
+		assert self.dbPath
+		# We split the SHA1 path in groups of 5
+		assert len(sha1) == 40
+		assert len(sha1) % 5 == 0
+		return "/".join(sha1[o:o+5] for o in range(0,len(sha1), 5))
 
 	def clean( self ):
-		for p in self.listPaths():
-			pass
-			#os.unlink(p)
+		pass
+		# for p in self.listPaths():
+		# 	pass
+		# 	#os.unlink(p)
 
 	def onFile( self, path, type, index ):
 		if type == "F":
-			h = self.sha1(path)
-			# TODO: We should split the file name instead so that
-			# we create subfolders 3/4/REST
-			p = os.path.join(self.dbPath, h) + ".lst"
+			# We split the SHA1 path in groups of 5
+			# We get the file name
+			p = os.path.join(self.dbPath, self.getPathForSHA1(self.sha1sum(path))) + ".lst"
+			# And the dirname, which we make sure exists
+			d = os.path.dirname(p)
+			if not os.path.exists(d):
+				os.makedirs(d)
+			# And we register the fil in the entry
 			with open(p, "at") as f:
 				f.write(path)
 				f.write("\n")
 		return True
 
-	def sha1( self, path ):
+	def sha1sum( self, path ):
+		"""Returns the SHA1 of the given path."""
 		with open(path, "rb") as f:
 			return hashlib.sha1(f.read()).hexdigest()
 
@@ -68,7 +90,6 @@ class Dedup(CatalogueReader):
 		inodes_path = {}
 		path_inode  = {}
 		#self.logging.info("Dedup: {0} [1+{1}]".format(paths[0], len(paths[1:])))
-		print("Dedup: {0} [1+{1}]".format(paths[0], len(paths[1:])))
 		# We iterate through the path, and  get the inode with the oldest
 		# mtime
 		for p in paths:
@@ -90,13 +111,18 @@ class Dedup(CatalogueReader):
 		original_inode = sorted_inodes[0][0]
 		original_path  = inodes_path[original_inode]
 		# Now we iterate back on the paths and re-create the symlinks
+		to_dedup = []
 		for p in paths:
 			if p != original_path:
 				if path_inode[p] != original_inode:
-					print ("Hardlink {0}→{1}".format(original_path, p))
-					os.unlink(p)
-					os.link(original_path, p, follow_symlinks=False)
-					self.copyattr(original_path, p)
+					to_dedup.append(p)
+		if to_dedup:
+			print("Dedup: {0} [1+{1}/{2}]".format(paths[0], len(to_dedup), len(paths[1:])))
+			for p in to_dedup:
+				print(" - {0}".format(p))
+				os.unlink(p)
+				os.link(original_path, p, follow_symlinks=False)
+				self.copyattr(original_path, p)
 
 	def copyattr( self, source, destination, stats=None ):
 		"""Copies the attributes from source to destination, (re)using the
